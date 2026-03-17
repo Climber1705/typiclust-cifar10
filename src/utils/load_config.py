36d4d2140ef7308e9ab00da2bc91aa1f6fbbaea6
Thomas <thomas.trotter2006@gmail.com>
2026-03-17 21:54:27 +0000

    Add load_config.py

---
+++ src/utils/load_config.py
@@ -0,0 +1,32 @@
+import torch
+from pathlib import Path
+
+from configs import (
+    Config,
+    AugmentationConfig,
+    TrainingConfig,
+    EvaluationConfig,
+    ClusterConfig,
+    Configs,
+)
+from utils import load_yaml
+
+def load_config(config_path: Path) -> Configs:
+    data = load_yaml(Path(config_path))
+
+    config_data = dict(data["config"])
+    if config_data.get("device") is None:
+        config_data["device"] = "cuda" if torch.cuda.is_available() else "cpu"
+
+    augmentation_data = dict(data["augmentation"])
+    augmentation_data["scale"] = tuple(augmentation_data["scale"])
+    augmentation_data["mean"] = tuple(augmentation_data["mean"])
+    augmentation_data["std"] = tuple(augmentation_data["std"])
+
+    config = Config(**config_data)
+    augmentation_config = AugmentationConfig(**augmentation_data)
+    training_config = TrainingConfig(**data["training"])
+    evaluation_config = EvaluationConfig(**data["evaluation"])
+    cluster_config = ClusterConfig(**data["cluster"])
+
+    return Configs(config, augmentation_config, training_config, evaluation_config, cluster_config)
