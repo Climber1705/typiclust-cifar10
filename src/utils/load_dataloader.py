7011475ffd80045242cb5f4407c90631c90d72b0
Thomas <thomas.trotter2006@gmail.com>
2026-03-17 22:03:15 +0000

    Add load_dataloader.py

---
+++ src/utils/load_dataloader.py
@@ -0,0 +1,17 @@
+from torch.utils.data import DataLoader, Dataset
+
+def build_loader(
+    dataset: Dataset,
+    batch_size: int,
+    shuffle: bool,
+    drop_last: bool = False
+) -> DataLoader:
+    return DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        num_workers=2,
+        persistent_workers=True,
+        pin_memory=True,
+        drop_last=drop_last,
+    )
