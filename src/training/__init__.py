b4c14fb85a4fb05a75caaeff1cc7b753a8674fde
Thomas <thomas.trotter2006@gmail.com>
2026-03-18 15:20:00 +0000

    Update __init__.py

--- src/training/__init__.py
+++ src/training/__init__.py
@@ -1,5 +1,7 @@
+from .embedding import build_embeddings
 from .simclr_train import train_simclr
 
 __all__ = [
+    "build_embeddings",
     "train_simclr",
 ]
--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,10 +1,12 @@
 from .load_config import load_config
 from .load_dataloader import build_loader
+from .load_model import load_model
 from .load_yaml import load_yaml
 from .seed import set_seed
 
 __all__ = [
     "load_config",
+    "load_model",
     "load_yaml",
     "set_seed",
     "build_loader"
