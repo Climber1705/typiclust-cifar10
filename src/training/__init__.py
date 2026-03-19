198291011cdf86bb013b71a81a7edebbf0a14ee6
Thomas <thomas.trotter2006@gmail.com>
2026-03-19 14:54:02 +0000

    Update __init__.py

--- src/training/__init__.py
+++ src/training/__init__.py
@@ -1,9 +1,16 @@
 from .simclr import build_embeddings, train_simclr
-from .classifier import train_classifier, evaluate_classifier
+from .classifier import (
+    train_classifier,
+    evaluate_classifier,
+    build_resnet18_classifier,
+    add_dropout_to_resnet,
+)
 
 __all__ = [
     "build_embeddings",
     "train_simclr",
     "train_classifier",
     "evaluate_classifier",
+    "build_resnet18_classifier",
+    "add_dropout_to_resnet",
 ]
--- src/training/classifier/__init__.py
+++ src/training/classifier/__init__.py
@@ -1,7 +1,9 @@
 from .evaluate import evaluate_classifier
 from .train import train_classifier
-
+from .utils import build_resnet18_classifier, add_dropout_to_resnet
 __all__ = [
     "evaluate_classifier",
     "train_classifier",
+    "build_resnet18_classifier",
+    "add_dropout_to_resnet",
 ]
