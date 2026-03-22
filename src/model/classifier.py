17f7ef84f901cbd887d31f66c196202b613cb1bf
Thomas <thomas.trotter2006@gmail.com>
2026-03-22 21:16:44 +0000

    Update classifier.py

--- src/model/classifier.py
+++ src/model/classifier.py
@@ -7,6 +7,8 @@ def build_resnet18_classifier(
     num_classes: int = 10
 ) -> nn.Module:
     model = torchvision.models.resnet18(weights=None)
+    model.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+    model.maxpool = nn.Identity()
     model.fc = nn.Linear(model.fc.in_features, num_classes)
     model.to(device)
     return model
