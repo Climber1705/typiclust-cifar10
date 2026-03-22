c2e361106f7acb15f3628ff9c00242a641034b32
Thomas <thomas.trotter2006@gmail.com>
2026-03-22 13:30:50 +0000

    Update model

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,7 +1,10 @@
+from .classifier import build_resnet18_classifier, add_dropout_to_resnet
 from .loader import load_model
 from .simclr import SimCLRModel, SimCLRLoss
 
 __all__ = [
+    "build_resnet18_classifier",
+    "add_dropout_to_resnet",
     "load_model",
     "SimCLRModel",
     "SimCLRLoss",
