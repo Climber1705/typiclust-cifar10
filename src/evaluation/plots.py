ed96e570e75366d61f3e89472f114c290c187dd9
Thomas <thomas.trotter2006@gmail.com>
2026-03-26 13:54:47 +0000

    Update plots.py

--- src/evaluation/plots.py
+++ src/evaluation/plots.py
@@ -3,6 +3,7 @@ import pandas as pd
 from pathlib import Path
 
 NAME_COLOURS = {
+    "BudgetAdaptiveTypiClust": "purple",
     "TPC_RP": "blue",           
     "Random": "black",
     "Uncertainty": "red",
