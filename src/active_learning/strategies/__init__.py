43b50ee1e19e1505a602c4f53bd20cc76730e0a5
Thomas <thomas.trotter2006@gmail.com>
2026-03-26 10:00:48 +0000

    Update __init__.py

--- src/active_learning/strategies/__init__.py
+++ src/active_learning/strategies/__init__.py
@@ -1,5 +1,6 @@
 from .badge import BADGEStrategy
 from .bald import BALDStrategy
+from .budget_adaptive_typiclust import BudgetAdaptiveTypiClust
 from .coreset import CoreSetStrategy
 from .dbal import DBALStrategy
 from .entropy import EntropyStrategy
@@ -11,6 +12,7 @@ from .uncertainty import UncertaintyStrategy
 __all__ = [
     "BADGEStrategy",
     "BALDStrategy",
+    "BudgetAdaptiveTypiClust",
     "CoreSetStrategy",
     "DBALStrategy",
     "EntropyStrategy",
