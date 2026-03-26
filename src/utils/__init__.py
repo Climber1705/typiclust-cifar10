c7bfafad7322a8b86d3ade262c5996f62586b3fb
Thomas <thomas.trotter2006@gmail.com>
2026-03-26 14:38:41 +0000

    Update __init__.py

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,9 +1,10 @@
 from .dataloaders import build_loader
-from .paths import get_directories
+from .paths import get_directories, get_project_root
 from .reproducibility import set_seed
 
 __all__ = [
     "build_loader",
     "get_directories",
+    "get_project_root",
     "set_seed",
 ]
