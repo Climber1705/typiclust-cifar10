9831053cf0f79cb975626d46b402d439d24dd567
Thomas <thomas.trotter2006@gmail.com>
2026-03-26 14:38:51 +0000

    Update paths.py

--- src/utils/paths.py
+++ src/utils/paths.py
@@ -3,8 +3,11 @@ from typing import Tuple
 
 from src.config import Config
 
+def get_project_root() -> Path:
+    return Path(__file__).resolve().parent.parent.parent
+
 def get_directories(config: Config) -> Tuple[Path, Path]:
-    project_root = Path(__file__).resolve().parent.parent.parent
+    project_root = get_project_root()
     data_directory = (project_root / config.paths.data_directory).resolve()
     model_directory = (project_root / config.paths.model_directory).resolve()
     return data_directory, model_directory
